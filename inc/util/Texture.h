a2f06ef379134ee8f319b493ada01c6401cd03f9
Alec Divito <divito.alec@gmail.com>
2017-08-31 18:44:02 -0400

    Added Texture class

---
+++ inc/util/Texture.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/******************************************************************************
+ *
+ * Texture.h
+ * Alec Divito
+ * divito.alec@gmail.com
+ * Thu Aug 31 18:43:05 EDT 2017
+ * {description}
+ *
+ *****************************************************************************/
+
+class Texture
+{
+    public:
+       Texture();
+       virtual ~Texture();
+    protected:
+    private:
+};
---
+++ src/util/Texture.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *
+ * Texture.cpp
+ * Alec Divito
+ * divito.alec@gmail.com
+ * Thu Aug 31 18:43:05 EDT 2017
+ * {description}
+ *
+ *****************************************************************************/
+
+#include "Texture.h"
+
+Texture::Texture()
+{
+    // ctor
+}
+
+
+Texture::~Texture()
+{
+    // dtor
+}
