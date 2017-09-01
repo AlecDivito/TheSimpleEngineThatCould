cae11dbc1bb653ca7f8732d3a89a4300a854b96e
Alec Divito <divito.alec@gmail.com>
2017-08-31 20:47:10 -0400

    Added First person camera class

---
+++ inc/controller/Camera3d.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/******************************************************************************
+ *
+ * Camera3d.h
+ * Alec Divito
+ * divito.alec@gmail.com
+ * Thu Aug 31 20:45:22 EDT 2017
+ * {description}
+ *
+ *****************************************************************************/
+
+class Camera3d
+{
+    public:
+       Camera3d();
+       virtual ~Camera3d();
+    protected:
+    private:
+};
---
+++ src/controller/Camera3d.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *
+ * Camera3d.cpp
+ * Alec Divito
+ * divito.alec@gmail.com
+ * Thu Aug 31 20:45:22 EDT 2017
+ * {description}
+ *
+ *****************************************************************************/
+
+#include "controller/Camera3d.h"
+
+Camera3d::Camera3d()
+{
+    // ctor
+}
+
+
+Camera3d::~Camera3d()
+{
+    // dtor
+}
