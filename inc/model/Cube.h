ee41b106ea52ff837ca46dc44360da5f1db4d293
Alec Divito <divito.alec@gmail.com>
2017-09-06 20:46:32 -0400

    Added Cube Class

---
+++ inc/model/Cube.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/******************************************************************************
+ *
+ * Cube.h
+ * Alec Divito
+ * divito.alec@gmail.com
+ * Wed Sep  6 20:46:13 EDT 2017
+ * {description}
+ *
+ *****************************************************************************/
+
+class Cube
+{
+    public:
+       Cube();
+       virtual ~Cube();
+    protected:
+    private:
+};
